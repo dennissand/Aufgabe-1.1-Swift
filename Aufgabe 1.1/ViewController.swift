6c9ad4a2991aef272ac3d0c13bcef732665ddf80
Dennis Sand <97163222+dennissand@users.noreply.github.com>
2022-08-15 11:16:17 +0200

    Initial Commit

---
+++ Aufgabe 1.1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Aufgabe 1.1
+//
+//  Created by Dennis Sand on 15.08.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
